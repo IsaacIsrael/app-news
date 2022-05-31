334b16d1aa1f78df3abd485ceec48a880ee01454
Isaac Israel <email.iisrael@gmail.com>
2022-05-31 20:14:04 +0300

    create Small component

---
+++ src/components/texts/Small.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, TextProps } from 'react-native';
+import { Color } from '../../constants/Color';
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 14,
+    letterSpacing: 0.25,
+    color: Color.TEXT,
+  },
+});
+
+const Small: React.FC<TextProps> = ({ style, ...props }) => {
+  return <Text style={[styles.text, style]} {...props} />;
+};
+
+export default Small;
