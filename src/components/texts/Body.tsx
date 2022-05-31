028839c51f095db7274bd333e18aebf84f11b1d3
Isaac Israel <email.iisrael@gmail.com>
2022-05-31 20:13:50 +0300

    create Body component

---
+++ src/components/texts/Body.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, TextProps } from 'react-native';
+import { Color } from '../../constants/Color';
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 22,
+    letterSpacing: 0.25,
+    color: Color.TEXT,
+  },
+});
+
+const Body: React.FC<TextProps> = ({ style, ...props }) => {
+  return <Text style={[styles.text, style]} {...props} />;
+};
+
+export default Body;
